d850e0df4b4f77d0095a502a5724a1e944f8ac6c
Jin-Jin Lee <a11913405@unet.univie.ac.at>
2021-12-11 00:09:50 +0100

    created Pacman class

--- lab1c/Pacman.js
+++ lab1c/Pacman.js
@@ -18,25 +18,31 @@ class Pacman {
         this.body.draw();
     }
 
-    /* translate(vector) {
-         mat4.translate(this.translationMatrix, this.translationMatrix, vector);
-    
-         mat4.multiply(this.helper, this.rotationMatrix, this.scaleMatrix);
-         mat4.multiply(this.modelMatrix, this.translationMatrix, this.helper);
-         //mat4.translate(this.modelMatrix, this.modelMatrix, vector);
-     }
-    
-     rotate(angle, axes) {
-         mat4.rotate(this.rotationMatrix, this.rotationMatrix, angle, axes);
-    
-         mat4.multiply(this.helper, this.rotationMatrix, this.scaleMatrix);
-         mat4.multiply(this.modelMatrix, this.translationMatrix, this.helper);
-     }
-    
-     scale(vector) {
-         mat4.scale(this.scaleMatrix, this.scaleMatrix, vector);
-    
-         mat4.multiply(this.helper, this.rotationMatrix, this.scaleMatrix);
-         mat4.multiply(this.modelMatrix, this.translationMatrix, this.helper);
-     }*/
+    translate(vector) {
+        this.head.translate(vector);
+        this.body.translate(vector);
+    }
+
+    rotate(angle, axes) {
+        this.head.rotate(angle, axes);
+        this.body.rotate(angle, axes);
+    }
+
+    scale(vector) {
+        this.head.scale(vector);
+        this.body.scale(vector);
+    }
+
+    turn(angle) {
+        this.head.rotate(angle, [0, 1, 0]);
+        this.body.rotate(angle, [0, 1, 0]);
+    }
+
+    openMouth(angle) {
+        this.head.rotate(angle, [1, 0, 0]);
+    }
+
+    closeMouth(angle) {
+        this.head.rotate(angle, [1, 0, 0]);
+    }
 }
